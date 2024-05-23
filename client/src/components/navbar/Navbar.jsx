5b29d8d4e5e25dd164c531470cfb052e7901972c
har <1900300100085@ipec.in>
2024-05-23 11:15:00 +0530

    Adding_Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.scss";
+import CustomButton from "../custom-button/CustomButton";
 
-const Navbar = () => {
+function App() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="container-nav">
+      <div className="navbar">
+        <div className="logo">LOGO</div>
+        <div className="menu">
+          <ul className="menu-options">
+            <li className="menu-item">
+              <a href="#">Prepare</a>
+            </li>
+            <li className="menu-item">
+              <a href="#">Certify</a>
+            </li>
+            <li className="menu-item">
+              <a href="#">Compete</a>
+            </li>
+            <li className="menu-item">
+              <a href="#">Apply</a>
+            </li>
+          </ul>
+        </div>
+        <div className="login-menu">
+          <CustomButton varient="filled" size="sm" className="login">
+            Log in
+          </CustomButton>
+          <CustomButton varient="filled-dark" size="sm" className="signup">
+            Sign Up
+          </CustomButton>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default App;
+
+// import React from "react";
+// import "./Navbar.scss";
+// import CustomButton from "../custom-button/CustomButton";
+
+// function App() {
+//   return (
+//     <div className="containner-nav">
+//       <div className="navbar">
+//         <div className="logo">LOGO</div>
+//         <div className="menu">
+//           <ul className="menu-options">
+//             <li className="menu-items">
+//               <a href="#">Prepare</a>
+//             </li>
+//             <li className="menu-items">
+//               <a href="#">Certify</a>
+//             </li>
+//             <li className="menu-items">
+//               <a href="#">Compete</a>
+//             </li>
+//             <li className="menu-items">
+//               <a href="#">Apply</a>
+//             </li>
+//           </ul>
+//         </div>
+//         <div className="loginmenu">
+//           <CustomButton varient="filled" size="sm" className="login">
+//             Log in
+//           </CustomButton>
+//           <CustomButton size="sm" varient="filled-dark" className="singup">
+//             Sing Up
+//           </CustomButton>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
